3ead3d8a52453d517cece7381ad9d40bb8a2546c
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2020-05-03 13:04:29 +0100

    check binaries exist

--- test.sh
+++ test.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# Check that non-standard programs are installed. "standard" programs are
+# anything that is specified in the POSIX.1-2008 standard (and the IEEE Std
+# 1003.1 standard) or that is a BASH builtin command. Therefore, "non-standard"
+# programs are anything that does not appear on the following two lists:
+#   * https://pubs.opengroup.org/onlinepubs/9699919799/idx/utilities.html
+#   * https://www.gnu.org/software/bash/manual/html_node/Bash-Builtins.html
+
 # Define binaries and check that they exist ...
 python="python3.7"
 pylint="pylint-3.7"
