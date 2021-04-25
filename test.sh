831f4ae8df38b5be54844edd6503dec8699b2d80
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2021-04-25 12:42:58 +0100

    use printf to align text; be explicit when using grep; protect variables with {}

--- test.sh
+++ test.sh
@@ -15,18 +15,18 @@ fi
 pylint="/opt/local/bin/pylint-3.8"
 python="/opt/local/bin/python3.8"
 shellcheck="/opt/local/bin/shellcheck"
-if ! type "$pylint" &> /dev/null; then
+if ! type "${pylint}" &> /dev/null; then
     echo "WARNING: the binary defined in \$pylint does not exist (Python checks will be skipped)"
 fi
-if ! type "$python" &> /dev/null; then
+if ! type "${python}" &> /dev/null; then
     echo "WARNING: the binary defined in \$python does not exist (Python checks will be skipped)"
 fi
-if ! type "$shellcheck" &> /dev/null; then
+if ! type "${shellcheck}" &> /dev/null; then
     echo "WARNING: the binary defined in \$shellcheck does not exist (Shell checks will be skipped)"
 fi
 
 # Change directory ...
-cd "$HOME/Repositories" || exit 1
+cd "${HOME}/Repositories" || exit 1
 
 # Loop over directories ...
 for d in *; do
@@ -36,12 +36,12 @@ for d in *; do
     # **************************************************************************
 
     # Print warning if there isn't a PyLint configuration file ...
-    if ! type "$pylint" &> /dev/null; then
+    if ! type "${pylint}" &> /dev/null; then
         false
-    elif ! type "$python" &> /dev/null; then
+    elif ! type "${python}" &> /dev/null; then
         false
     elif [[ ! -f $d/.pylintrc ]]; then
-        echo "WARNING: $d is missing a PyLint configuration file"
+        echo "WARNING: \"${d}\" is missing a PyLint configuration file"
     else
         # Check if it is a Python module or if it is just a directory of Python
         # scripts ...
@@ -53,86 +53,86 @@ for d in *; do
                 continue
             fi
 
-            echo -n "Testing \"$d\" (as a Python module): "
+            printf "%-60s : " "Testing \"${d}\" (as a Python module)"
 
             # Clean then import the Python module ...
-            find "$d" -type f -name "*.pyc" -delete
-            find "$d" -type d -name "__pycache__" -delete
-            if ! $python -c "import $d" &> /dev/null; then
+            find "${d}" -type f -name "*.pyc" -delete
+            find "${d}" -type d -name "__pycache__" -delete
+            if ! ${python} -c "import ${d}" &> /dev/null; then
                 echo ""
-                echo "ERROR: Failed to import \"$d\"" >&2
+                echo "ERROR: Failed to import \"${d}\"" >&2
                 exit 1
             fi
 
             # Run PyLint on the Python module ...
-            $pylint --rcfile="$d/.pylintrc" "$d" &> "$d/pylint.log"
+            ${pylint} --rcfile="${d}/.pylintrc" "${d}" &> "${d}/pylint.log"
 
             # Check if it is perfect ...
-            if grep "Your code has been rated at 10.00/10" "$d/pylint.log" &> /dev/null; then
+            if grep -F "Your code has been rated at 10.00/10" "${d}/pylint.log" &> /dev/null; then
                 echo "perfect"
             else
-                grep "Your code has been rated at" "$d/pylint.log" | cut -c 29-
+                grep -F "Your code has been rated at" "${d}/pylint.log" | cut -c 29-
             fi
         else
             # Try to find all of the Python scripts that are not part of Git
             # submodules ...
             tmp1="$(mktemp)"
             tmp2="$(mktemp)"
-            find "$d" -type f -name "*.py" | grep -v "/build/" | sort > "$tmp1"
+            find "${d}" -type f -name "*.py" | grep -v -F "/build/" | sort > "${tmp1}"
             if [[ -f $d/.gitmodules ]]; then
-                for m in $(grep "path = " "$d/.gitmodules" | cut -d "=" -f 2); do
-                    grep -v -E "^$d/$m" "$tmp1" > "$tmp2"
-                    cp "$tmp2" "$tmp1"
+                for m in $(grep -F "path = " "${d}/.gitmodules" | cut -d "=" -f 2); do
+                    grep -v -E "^${d}/${m}" "${tmp1}" > "${tmp2}"
+                    cp "${tmp2}" "${tmp1}"
                 done
             fi
 
             # Check that there are some Python scripts ...
-            if [[ $(wc -l < "$tmp1") -gt 0 ]]; then
-                echo -n "Testing \"$d\" (as a Python script directory): "
+            if [[ $(wc -l < "${tmp1}") -gt 0 ]]; then
+                printf "%-60s : " "Testing \"${d}\" (as a Python script directory)"
 
                 # Run PyLint on the Python script directory ...
-                $pylint --rcfile="$d/.pylintrc" $(cat "$tmp1") &> "$d/pylint.log"
+                ${pylint} --rcfile="${d}/.pylintrc" $(cat "${tmp1}") &> "${d}/pylint.log"
 
                 # Check if it is perfect ...
-                if grep "Your code has been rated at 10.00/10" "$d/pylint.log" &> /dev/null; then
+                if grep -F "Your code has been rated at 10.00/10" "${d}/pylint.log" &> /dev/null; then
                     echo "perfect"
                 else
-                    grep "Your code has been rated at" "$d/pylint.log" | cut -c 29-
+                    grep -F "Your code has been rated at" "${d}/pylint.log" | cut -c 29-
                 fi
             fi
 
             # Clean up ...
-            rm "$tmp1" "$tmp2"
+            rm "${tmp1}" "${tmp2}"
         fi
     fi
 
     # **************************************************************************
 
     # Print warning if there isn't a ShellCheck configuration file ...
-    if ! type "$shellcheck" &> /dev/null; then
+    if ! type "${shellcheck}" &> /dev/null; then
         false
     elif [[ ! -f $d/.shellcheckrc ]]; then
-        echo "WARNING: $d is missing a ShellCheck configuration file"
+        echo "WARNING: \"${d}\" is missing a ShellCheck configuration file"
     else
         # Try to find all of the Shell scripts that are not part of Git
         # submodules ...
         tmp1="$(mktemp)"
         tmp2="$(mktemp)"
-        find "$d" -type f -name "*.sh" | grep -v "/build/" | sort > "$tmp1"
+        find "${d}" -type f -name "*.sh" | grep -v -F "/build/" | sort > "${tmp1}"
         if [[ -f $d/.gitmodules ]]; then
-            for m in $(grep "path = " "$d/.gitmodules" | cut -d "=" -f 2); do
-                grep -v -E "^$d/$m" "$tmp1" > "$tmp2"
-                cp "$tmp2" "$tmp1"
+            for m in $(grep -F "path = " "${d}/.gitmodules" | cut -d "=" -f 2); do
+                grep -v -E "^${d}/${m}" "${tmp1}" > "${tmp2}"
+                cp "${tmp2}" "${tmp1}"
             done
         fi
 
         # Check that there are some Shell scripts ...
-        if [[ $(wc -l < "$tmp1") -gt 0 ]]; then
-            echo -n "Testing \"$d\" (as a Shell script directory): "
+        if [[ $(wc -l < "${tmp1}") -gt 0 ]]; then
+            printf "%-60s : " "Testing \"${d}\" (as a Shell script directory)"
 
             # Run ShellCheck on the Shell script directory ...
-            ln -sf "$d/.shellcheckrc" ".shellcheckrc"
-            if $shellcheck -x $(cat "$tmp1") &> "$d/shellcheck.log"; then
+            ln -sf "${d}/.shellcheckrc" ".shellcheckrc"
+            if ${shellcheck} -x $(cat "${tmp1}") &> "${d}/shellcheck.log"; then
                 echo "perfect"
             else
                 echo "has issues"
@@ -141,6 +141,6 @@ for d in *; do
         fi
 
         # Clean up ...
-        rm "$tmp1" "$tmp2"
+        rm "${tmp1}" "${tmp2}"
     fi
 done
