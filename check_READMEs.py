8a27e995918c38f92b8aaae5380efee0dfb4a98a
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2022-12-04 19:42:34 +0000

    use the proper idiom in the main module

--- check_READMEs.py
+++ check_READMEs.py
@@ -1,104 +1,109 @@
 #!/usr/bin/env python3
 
-# Import standard modules ...
-import glob
-import json
-import os
+# Use the proper idiom in the main module ...
+# NOTE: See https://docs.python.org/3.10/library/multiprocessing.html#the-spawn-and-forkserver-start-methods
+if __name__ == "__main__":
+    # Import standard modules ...
+    import glob
+    import json
+    import os
 
-# Import my modules ...
-try:
-    import pyguymer3
-except:
-    raise Exception("\"pyguymer3\" is not installed; you need to have the Python module from https://github.com/Guymer/PyGuymer3 located somewhere in your $PYTHONPATH") from None
-
-# Load list of standard modules ...
-with open(f"{os.path.dirname(os.path.realpath(__file__))}/Python 3.10 Standard Modules.json", "rt", encoding = "utf-8") as fobj:
-    standardModules = json.load(fobj)
-
-# Append common non-standard modules that I do not want to be warned about ...
-standardModules.append("f90")
-standardModules.append("funcs")
-standardModules.append("sphinx_rtd_theme")
-standardModules.append("web_mod")
-
-# Loop over READMEs ...
-for rname in sorted(glob.glob("*/README.md")):
-    print(f"Checking \"{rname}\" ...")
-
-    # Deduce directory name ...
-    dname = os.path.dirname(rname)
+    # Import my modules ...
+    try:
+        import pyguymer3
+    except:
+        raise Exception("\"pyguymer3\" is not installed; you need to have the Python module from https://github.com/Guymer/PyGuymer3 located somewhere in your $PYTHONPATH") from None
 
     # **************************************************************************
 
-    # Initialize list of sub-paths to ignore ...
-    ipaths = [
-        f"{dname}/.git",
-        f"{dname}/build",
-    ]
+    # Load list of standard modules ...
+    with open(f"{os.path.dirname(os.path.realpath(__file__))}/Python 3.10 Standard Modules.json", "rt", encoding = "utf-8") as fobj:
+        standardModules = json.load(fobj)
 
-    # Deduce .gitmodules file name, check if it exists and populate list with
-    # paths to ignore if it does ...
-    mname = f"{dname}/.gitmodules"
-    if os.path.exists(mname):
-        with open(mname, "rt", encoding = "utf-8") as fobj:
-            for line in fobj:
-                if not line.strip().startswith("path = "):
-                    continue
-                ipaths.append(f'{dname}/{"=".join(line.strip().split("=")[1:]).strip()}')
+    # Append common non-standard modules that I do not want to be warned about ...
+    standardModules.append("f90")
+    standardModules.append("funcs")
+    standardModules.append("sphinx_rtd_theme")
+    standardModules.append("web_mod")
 
-    # **************************************************************************
+    # Loop over READMEs ...
+    for rname in sorted(glob.glob("*/README.md")):
+        print(f"Checking \"{rname}\" ...")
 
-    # Initialize list of imported non-standard modules ...
-    modules = []
+        # Deduce directory name ...
+        dname = os.path.dirname(rname)
 
-    # Loop over files in directory ...
-    for pname in pyguymer3.return_file_list(dname):
-        # Skip if the file is not a Python script ...
-        if not pname.endswith(".py"):
+        # **********************************************************************
+
+        # Initialize list of sub-paths to ignore ...
+        ipaths = [
+            f"{dname}/.git",
+            f"{dname}/build",
+        ]
+
+        # Deduce .gitmodules file name, check if it exists and populate list
+        # with paths to ignore if it does ...
+        mname = f"{dname}/.gitmodules"
+        if os.path.exists(mname):
+            with open(mname, "rt", encoding = "utf-8") as fobj:
+                for line in fobj:
+                    if not line.strip().startswith("path = "):
+                        continue
+                    ipaths.append(f'{dname}/{"=".join(line.strip().split("=")[1:]).strip()}')
+
+        # **********************************************************************
+
+        # Initialize list of imported non-standard modules ...
+        modules = []
+
+        # Loop over files in directory ...
+        for pname in pyguymer3.return_file_list(dname):
+            # Skip if the file is not a Python script ...
+            if not pname.endswith(".py"):
+                continue
+
+            # Skip if the file is in a directory which is to be ignored ...
+            ignore = False
+            for ipath in ipaths:
+                if pname.startswith(ipath):
+                    ignore = True
+                    break
+            if ignore:
+                continue
+
+            # Find all the non-standard modules which are imported and append
+            # the missing ones to the list ...
+            with open(pname, "rt", encoding = "utf-8") as fobj:
+                for line in fobj:
+                    if not line.strip().startswith("import "):
+                        continue
+                    module = line.strip().split(" ")[1].split(".")[0]
+                    if module == dname:
+                        continue
+                    if module in standardModules:
+                        continue
+                    if module in modules:
+                        continue
+                    modules.append(module)
+
+        # Skip this README if the repository does not import any non-standard
+        # modules ...
+        if len(modules) == 0:
             continue
 
-        # Skip if the file is in a directory which is to be ignored ...
-        ignore = False
-        for ipath in ipaths:
-            if pname.startswith(ipath):
-                ignore = True
-                break
-        if ignore:
-            continue
+        # **********************************************************************
 
-        # Find all the non-standard modules which are imported and append the
-        # missing ones to the list ...
-        with open(pname, "rt", encoding = "utf-8") as fobj:
-            for line in fobj:
-                if not line.strip().startswith("import "):
-                    continue
-                module = line.strip().split(" ")[1].split(".")[0]
-                if module == dname:
-                    continue
-                if module in standardModules:
-                    continue
-                if module in modules:
-                    continue
-                modules.append(module)
+        # Load README ...
+        with open(rname, "rt", encoding = "utf-8") as fobj:
+            lines = fobj.readlines()
 
-    # Skip this README if the repository does not import any non-standard
-    # modules ...
-    if len(modules) == 0:
-        continue
-
-    # **************************************************************************
-
-    # Load README ...
-    with open(rname, "rt", encoding = "utf-8") as fobj:
-        lines = fobj.readlines()
-
-    # Loop over imported non-standard modules ...
-    for module in sorted(modules):
-        linked = False
-        for line in lines:
-            if line.strip().startswith(f"* [{module}]("):
-                linked = True
-                break
-        if linked:
-            continue
-        print(f"  ... needs a link adding for \"{module}\"")
+        # Loop over imported non-standard modules ...
+        for module in sorted(modules):
+            linked = False
+            for line in lines:
+                if line.strip().startswith(f"* [{module}]("):
+                    linked = True
+                    break
+            if linked:
+                continue
+            print(f"  ... needs a link adding for \"{module}\"")
--- check_allKwArgs.py
+++ check_allKwArgs.py
@@ -1,122 +1,125 @@
 #!/usr/bin/env python3
 
-# Import standard modules ...
-import argparse
-import ast
+# Use the proper idiom in the main module ...
+# NOTE: See https://docs.python.org/3.10/library/multiprocessing.html#the-spawn-and-forkserver-start-methods
+if __name__ == "__main__":
+    # Import standard modules ...
+    import argparse
+    import ast
 
-# Import my modules ...
-try:
-    import pyguymer3
-except:
-    raise Exception("\"pyguymer3\" is not installed; you need to have the Python module from https://github.com/Guymer/PyGuymer3 located somewhere in your $PYTHONPATH") from None
+    # Import my modules ...
+    try:
+        import pyguymer3
+    except:
+        raise Exception("\"pyguymer3\" is not installed; you need to have the Python module from https://github.com/Guymer/PyGuymer3 located somewhere in your $PYTHONPATH") from None
 
-# ******************************************************************************
+    # **************************************************************************
 
-# Create argument parser and parse the arguments ...
-parser = argparse.ArgumentParser(
-       allow_abbrev = False,
-        description = "Check keyword arguments in Python functions.",
-    formatter_class = argparse.ArgumentDefaultsHelpFormatter,
-)
-parser.add_argument(
-    "dname",
-    help = "the folder to check",
-)
-args = parser.parse_args()
+    # Create argument parser and parse the arguments ...
+    parser = argparse.ArgumentParser(
+           allow_abbrev = False,
+            description = "Check keyword arguments in Python functions.",
+        formatter_class = argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "dname",
+        help = "the folder to check",
+    )
+    args = parser.parse_args()
 
-# ******************************************************************************
+    # **************************************************************************
 
-# Initialize database ...
-funcs = {}
+    # Initialize database ...
+    funcs = {}
 
-# Loop over files in folder ...
-for fname in pyguymer3.return_file_list(args.dname):
-    # Skip files which are not Python scripts ...
-    if not fname.endswith(".py"):
-        continue
-
-    # Load the Python script ...
-    with open(fname, "rt", encoding = "utf-8") as fObj:
-        src = fObj.read()
-
-    # Parse the Python script ...
-    tree = ast.parse(src)
-
-    # Loop over bodies ...
-    for body in tree.body:
-        # Skip bodies which are not function definitions ...
-        if not isinstance(body, ast.FunctionDef):
+    # Loop over files in folder ...
+    for fname in pyguymer3.return_file_list(args.dname):
+        # Skip files which are not Python scripts ...
+        if not fname.endswith(".py"):
             continue
 
-        # Find out how many positional and keyword arguments there are, and skip
-        # if there aren't any ...
-        nPosArgs = len(body.args.args)
-        nKwArgs = len(body.args.defaults)
-        if nPosArgs == 0:
-            continue
-        if nKwArgs == 0:
-            continue
+        # Load the Python script ...
+        with open(fname, "rt", encoding = "utf-8") as fObj:
+            src = fObj.read()
 
-        # Loop over keyword arguments ...
-        for iKwArg in range(nKwArgs):
-            # Deduce index to positional argument ...
-            iPosArg = nPosArgs - nKwArgs + iKwArg
+        # Parse the Python script ...
+        tree = ast.parse(src)
 
-            # Skip if this keyword argument is the positional argument check ...
-            if body.args.args[iPosArg].arg in ("kwArgCheck",):
+        # Loop over bodies ...
+        for body in tree.body:
+            # Skip bodies which are not function definitions ...
+            if not isinstance(body, ast.FunctionDef):
                 continue
 
-            # Add this function to the database ...
-            if body.name not in funcs:
-                funcs[body.name] = []
-
-            # Append this keyword argument to the database ...
-            funcs[body.name].append(body.args.args[iPosArg].arg)
-
-# ******************************************************************************
-
-# Loop over files in folder ...
-for fname in pyguymer3.return_file_list(args.dname):
-    # Skip files which are not Python scripts ...
-    if not fname.endswith(".py"):
-        continue
-
-    # Load the Python script ...
-    with open(fname, "rt", encoding = "utf-8") as fObj:
-        src = fObj.read()
-
-    # Parse the Python script ...
-    tree = ast.parse(src)
-
-    # Loop over bodies ...
-    for body in tree.body:
-        # Skip bodies which are not function definitions ...
-        if not isinstance(body, ast.FunctionDef):
-            continue
-
-        # Loop over nodes in the body ...
-        for node in ast.walk(body):
-            # Skip this node if it is not a function call (or if it is a member
-            # of a module) ...
-            if not isinstance(node, ast.Call):
+            # Find out how many positional and keyword arguments there are, and
+            # skip if there aren't any ...
+            nPosArgs = len(body.args.args)
+            nKwArgs = len(body.args.defaults)
+            if nPosArgs == 0:
                 continue
-            if not isinstance(node.func, ast.Name):
+            if nKwArgs == 0:
                 continue
 
-            # Skip this function call if it is not in the database ...
-            if node.func.id not in funcs:
-                continue
+            # Loop over keyword arguments ...
+            for iKwArg in range(nKwArgs):
+                # Deduce index to positional argument ...
+                iPosArg = nPosArgs - nKwArgs + iKwArg
 
-            # Find all the keyword arguments that are passed ...
-            keywords = []
-            for keyword in node.keywords:
-                keywords.append(keyword.arg)
-
-            # Loop over keyword arguments that *should* be passed ...
-            for keyword in funcs[node.func.id]:
-                # Skip this keyword argument if it is passed ...
-                if keyword in keywords:
+                # Skip if this keyword argument is the positional argument check ...
+                if body.args.args[iPosArg].arg in ("kwArgCheck",):
                     continue
 
-                # Print ...
-                print(f"\"{fname}\" calls \"{node.func.id}()\" but does not pass \"{keyword}\"")
+                # Add this function to the database ...
+                if body.name not in funcs:
+                    funcs[body.name] = []
+
+                # Append this keyword argument to the database ...
+                funcs[body.name].append(body.args.args[iPosArg].arg)
+
+    # **************************************************************************
+
+    # Loop over files in folder ...
+    for fname in pyguymer3.return_file_list(args.dname):
+        # Skip files which are not Python scripts ...
+        if not fname.endswith(".py"):
+            continue
+
+        # Load the Python script ...
+        with open(fname, "rt", encoding = "utf-8") as fObj:
+            src = fObj.read()
+
+        # Parse the Python script ...
+        tree = ast.parse(src)
+
+        # Loop over bodies ...
+        for body in tree.body:
+            # Skip bodies which are not function definitions ...
+            if not isinstance(body, ast.FunctionDef):
+                continue
+
+            # Loop over nodes in the body ...
+            for node in ast.walk(body):
+                # Skip this node if it is not a function call (or if it is a
+                # member of a module) ...
+                if not isinstance(node, ast.Call):
+                    continue
+                if not isinstance(node.func, ast.Name):
+                    continue
+
+                # Skip this function call if it is not in the database ...
+                if node.func.id not in funcs:
+                    continue
+
+                # Find all the keyword arguments that are passed ...
+                keywords = []
+                for keyword in node.keywords:
+                    keywords.append(keyword.arg)
+
+                # Loop over keyword arguments that *should* be passed ...
+                for keyword in funcs[node.func.id]:
+                    # Skip this keyword argument if it is passed ...
+                    if keyword in keywords:
+                        continue
+
+                    # Print ...
+                    print(f"\"{fname}\" calls \"{node.func.id}()\" but does not pass \"{keyword}\"")
