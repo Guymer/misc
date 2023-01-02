ed8e02f2739b55c3f2cc2caabd5058af188d7755
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2023-01-02 12:51:10 +0000

    rename variable to camelCase

--- check_READMEs.py
+++ check_READMEs.py
@@ -17,8 +17,8 @@ if __name__ == "__main__":
     # **************************************************************************
 
     # Load list of standard modules ...
-    with open(f"{os.path.dirname(os.path.realpath(__file__))}/Python 3.10 Standard Modules.json", "rt", encoding = "utf-8") as fobj:
-        standardModules = json.load(fobj)
+    with open(f"{os.path.dirname(os.path.realpath(__file__))}/Python 3.10 Standard Modules.json", "rt", encoding = "utf-8") as fObj:
+        standardModules = json.load(fObj)
 
     # Append common non-standard modules that I do not want to be warned about ...
     standardModules.append("f90")
@@ -45,8 +45,8 @@ if __name__ == "__main__":
         # with paths to ignore if it does ...
         mname = f"{dname}/.gitmodules"
         if os.path.exists(mname):
-            with open(mname, "rt", encoding = "utf-8") as fobj:
-                for line in fobj:
+            with open(mname, "rt", encoding = "utf-8") as fObj:
+                for line in fObj:
                     if not line.strip().startswith("path = "):
                         continue
                     ipaths.append(f'{dname}/{"=".join(line.strip().split("=")[1:]).strip()}')
@@ -73,8 +73,8 @@ if __name__ == "__main__":
 
             # Find all the non-standard modules which are imported and append
             # the missing ones to the list ...
-            with open(pname, "rt", encoding = "utf-8") as fobj:
-                for line in fobj:
+            with open(pname, "rt", encoding = "utf-8") as fObj:
+                for line in fObj:
                     if not line.strip().startswith("import "):
                         continue
                     module = line.strip().split(" ")[1].split(".")[0]
@@ -94,8 +94,8 @@ if __name__ == "__main__":
         # **********************************************************************
 
         # Load README ...
-        with open(rname, "rt", encoding = "utf-8") as fobj:
-            lines = fobj.readlines()
+        with open(rname, "rt", encoding = "utf-8") as fObj:
+            lines = fObj.readlines()
 
         # Loop over imported non-standard modules ...
         for module in sorted(modules):
